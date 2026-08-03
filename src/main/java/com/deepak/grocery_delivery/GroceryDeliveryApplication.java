3acb61b59cd9f4c32a3ba5f2b1d322f8bb60d095
deepak-praveen-raj <dr4687242@gmail.com>
2026-08-04 00:58:01 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/deepak/grocery_delivery/GroceryDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.deepak.grocery_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepak/grocery_delivery/GroceryDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.grocery_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
