06502b1022e84f15106ee1f54fe14e031fb025d8
deepak-praveen-raj <dr4687242@gmail.com>
2026-08-12 10:33:24 +0530

    Create order repositories

---
+++ src/main/java/com/deepak/grocery_delivery/repository/OrderItemRepository.java
@@ -0,0 +1,4 @@
+package com.deepak.grocery_delivery.repository;
+
+public interface OrderItemRepository {
+}
---
+++ src/main/java/com/deepak/grocery_delivery/repository/OrderRepository.java
@@ -0,0 +1,15 @@
+package com.deepak.grocery_delivery.repository;
+
+import com.deepak.grocery_delivery.entity.Order;
+import com.deepak.grocery_delivery.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByUserOrderByCreatedAtDesc(User user);
+
+    Optional<Order> findByIdAndUser(Long id, User user);
+}
